69694cf1d31830fb32479669ee8794055f90b558
prithvihv <hvprithvi09@gmail.com>
2018-04-15 19:13:53 +0530

    deleted millions of stuff

--- app.js
+++ app.js
@@ -2,21 +2,7 @@ var express = require('express');
 
 var app = express();
 
-//ROUTE FILES
-var index = require('./routes/index');
-var fcm = require('./routes/FCM/fcm');
 
-//Routes
-app.use('/', index);
-app.use('/fcm', fcm);
-
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -27,6 +13,43 @@ app.use(function (err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.send('error');
+});
+
+
+function GroupInvitation_Notification(Registerationtoken, respon, GroupName, SenderName) {
+    var payload = {
+        notification: {
+            title: "Group Invitation",
+            body: "Invited to Group " + GroupName + "  by " + SenderName
+        }
+    };
+    var options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+    admin.messaging().sendToDevice(Registerationtoken, payload, options)
+        .then(function (response) {
+            console.log("Successfully sent message:", response);
+            respon.send(response);
+        })
+        .catch(function (error) {
+            console.log("Error sending message:", error);
+            respon.send(error);
+        });
+}
+
+
+//Routes
+router.get('/', function (req, res) {
+    res.send("hello world - from index");
+});
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.listen(7777, function () {
