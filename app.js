0043bf50f9004f8aade650e6ba9fdc6e0d63ddbe
prithvihv <hvprithvi09@gmail.com>
2018-04-15 19:22:39 +0530

    testing

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 var express = require('express');
-
 var app = express();
-
-
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.send('error');
-});
-
 
 function GroupInvitation_Notification(Registerationtoken, respon, GroupName, SenderName) {
     var payload = {
@@ -39,18 +24,12 @@ function GroupInvitation_Notification(Registerationtoken, respon, GroupName, Sen
 }
 
 
-//Routes
+
 app.get('/', function (req, res) {
     res.send("hello world - from index");
 });
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 app.listen(7777, function () {
     console.log("conerive FCM < server is running");
